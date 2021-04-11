6663a28699471547f229a3e552a75594780c32df
Jonas Oliveira <jonascsoliveira@outlook.com>
2021-04-10 23:49:11 -0300

    Create e update de contatos

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -4,11 +4,11 @@ exports.middlewareGlobal = (req, res, next) => {
   res.locals.user = req.session.user;
   next();
 };
-
+/*
 exports.outroMiddleware = (req, res, next) => {
   next();
 };
-
+*/
 exports.checkCsrfError = (err, req, res, next) => {
   if(err) {
     return res.render('404');
@@ -20,3 +20,13 @@ exports.csrfMiddleware = (req, res, next) => {
   res.locals.csrfToken = req.csrfToken();
   next();
 };
+
+exports.loginRequired = (req, res, next) => {
+  if (!req.session.user) {
+    req.flash('errors', 'Você precisa fazer login.');
+    req.session.save(()=> res.redirect('/'));
+    return;
+  }
+
+  next();
+}
---
+++ src/models/ContatoModel.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const ContatoSchema = new mongoose.Schema({
+  nome: { type: String, required: true },
+  sobrenome: { type: String, required: false, default: '' },
+  email: { type: String, required: false, default: '' },
+  telefone: { type: String, required: false, default: '' },
+  criadoEm: { type: Date, default: Date.now },
+});
+
+const ContatoModel = mongoose.model('Contato', ContatoSchema);
+//Fazendo com função construtora
+function Contato(body) {
+  this.body = body;
+  this.errors = [];
+  this.contato = null;
+}
+
+Contato.buscaPorId = async function(id){
+  if (typeof id !== 'string') return;
+  const user = await ContatoModel.findById(id);
+  return user;
+}
+
+Contato.prototype.register = async function(){
+  this.valida();
+
+  if (this.errors.length > 0) return;
+  this.contato = await ContatoModel.create(this.body);
+}
+
+Contato.prototype.valida = function(){
+  this.cleanUp();
+
+  if (!this.body.nome) this.errors.push('Nome é um campo obrigatório.');
+  if (this.body.email && !validator.isEmail(this.body.email)) this.errors.push('Email inválido');
+  if (!this.body.email && !this.body.telefone) {
+    this.errors.push('Pelo menos um contato é requerido: e-mail ou telefone.')
+  }
+}
+
+Contato.prototype.cleanUp = function(){
+  for (const key in this.body) {
+    if (typeof this.body[key] !== 'string') {
+      this.body[key] = '';
+    }
+  }
+
+  this.body = {
+    nome: this.body.nome,
+    sobrenome: this.body.sobrenome,
+    email: this.body.email,
+    telefone: this.body.telefone
+  };
+}
+
+Contato.prototype.edit = async function (id){
+  if(typeof id !== 'string') return;
+  this.valida();
+  if (this.errors.length > 0) return;
+  this.contato = await ContatoModel.findByIdAndUpdate(id, this.body, { new: true }) //parametro obj new:true para retornar objeto atualizando
+}
+
+module.exports = Contato;
