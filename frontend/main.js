3b682a37050b6359887db9ebe6bf1735dcfded3e
Jonas Oliveira <jonascsoliveira@outlook.com>
2021-04-11 19:21:01 -0300

    Correcao de bug

--- frontend/main.js
+++ frontend/main.js
@@ -6,6 +6,6 @@ import Login from './modules/Login';
 const login = new Login('.form-login');
 const cadastro = new Login('.form-cadastro');
 
-login.init();
-cadastro.init();
+//login.init();
+//cadastro.init();
 //import './assets/css/style.css';
